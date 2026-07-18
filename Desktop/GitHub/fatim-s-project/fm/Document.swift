713b680c960630e5680696d2f091a460ce4e0150
almahdoudfatimah-cell <almahdoudfatimah@gmail.com>
2026-07-18 12:46:39 +0300

    Add project files

---
+++ Desktop/GitHub/fatim-s-project/fm/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  fm
+//
+//  Created by Fatimah on 27/09/1447 AH.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
